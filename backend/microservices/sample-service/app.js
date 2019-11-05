9e61b77203c18f5f70eb8097fba3471eb1e138ed
Sai Baba Nadh Konda <saibabanadh@gmail.com>
2019-11-05 19:04:23 +0530

    Updated:sample-service with Dockerfile

--- backend/microservices/sample-service/app.js
+++ backend/microservices/sample-service/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const helmet = require('helmet');
 
 // Add custom dependencies
 const config = require('./config/config');
@@ -18,11 +19,24 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
     res.setHeader('Access-Control-Allow-Header', 'X-Requested-With, content-type, Authorization');
+    res.setTimeout(300000,function(){
+        res.status(408).json({success:false, message:"Request has timed out."})
+    })
     next();
 });
-
 app.use(cors());
+app.use(helmet.noCache())
+app.use(helmet.xssFilter());
+app.use(helmet.frameguard());
+app.use(helmet.hidePoweredBy());
+
+// Add service routes
 app.use('/sample', sampleRoutes);
+
+// Hanlde uncaughtExceptions here to prevent termination
+process.on('uncaughtException', (error) => {
+    console.log(error);
+});
 
 // Run the microservice app
 app.listen(config.PORT, () => {
