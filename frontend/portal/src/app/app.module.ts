15d23e7801495ac291f1b0e938e3c957ec35db07
Smriti Bauasaheb Jha <smriti.jha@incedoinc.com>
2019-11-06 11:58:14 +0530

    header and footer folder added

--- frontend/portal/src/app/app.module.ts
+++ frontend/portal/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DevicesModule } from 'devices';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
