03a1381a66956b28370fc0c347f7e3475a402d18
gavi755 <gavigadagi@gmail.com>
2019-11-08 11:55:15 +0530

    Create dashboard cards

--- frontend/portal/src/app/app.module.ts
+++ frontend/portal/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
+
 import { DevicesModule } from 'devices';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,8 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     DevicesModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
