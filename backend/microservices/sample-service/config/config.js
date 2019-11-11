ab510d6babf4d92012bb102dc02cb40f494ffbab
Sai Baba Nadh Konda <saibabanadh@gmail.com>
2019-11-11 16:36:33 +0530

    Updated: DB Connection & Swagger Documentation

--- backend/microservices/sample-service/config/config.js
+++ backend/microservices/sample-service/config/config.js
@@ -1,4 +1,12 @@
 module.exports = {
-	APP : "Sample App",
-	PORT : "3000"
+	LOCAL: true,
+	APP_NAME : "Sample App",
+	PORT : "3000",
+	MONGO : {
+		"hostname":"localhost",
+		"port":"27017",
+		"username":"",
+		"password":"",
+		"dbName": "sample"
+	}
 }
--- backend/microservices/sample-service/controllers/sample.controller.js
+++ backend/microservices/sample-service/controllers/sample.controller.js
@@ -1,6 +1,12 @@
 
-exports.home = async(req,res) => {
+exports.index = async(req,res) => {
 	res.status(200).json({
       message:"Welcome to Sample Microservice API"
+    });
+}
+
+exports.home = async(req,res) => {
+	res.status(200).json({
+      message:"Welcome to Sample Microservice API Home"
     });
 }
