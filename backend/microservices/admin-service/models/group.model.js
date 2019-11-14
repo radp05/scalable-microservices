26694ad247265535f6e04227fd24f2607b9b0527
Aman Kumar Singh <aman.ks@incedoinc.com>
2019-11-14 14:34:41 +0530

    feat - resource , resourceId

--- backend/microservices/admin-service/models/group.model.js
+++ backend/microservices/admin-service/models/group.model.js
@@ -4,7 +4,8 @@
 const mongoose = require('mongoose');
 
 const groupSchema = new mongoose.Schema({
-    groupName: { type: String }
+    groupName: { type: String, required: true },
+    resourceId: { type: mongoose.Schema.Types.ObjectId, required: true }
 });
 
 groupSchema.set('timestamps', true); // this will add createdAt and updatedAt timestamps
