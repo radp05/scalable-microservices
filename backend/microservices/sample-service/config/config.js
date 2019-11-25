3c285f1ff41dfce7e98907dac16af40f58f82573
Sai Baba Nadh Konda <saibabanadh@gmail.com>
2019-11-25 13:14:23 +0530

    Added: sample-service back

---
+++ backend/microservices/sample-service/config/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	LOCAL: false,
+	APP_NAME : "Sample App",
+	API_BASE : "/api/v1/sample",
+	PORT : "3000",
+	MONGO : {
+		"hostname":"localhost",
+		"port":"27017",
+		"username":"",
+		"password":"",
+		"dbName": "sample",
+		"replicaSet":null
+	}
+}
