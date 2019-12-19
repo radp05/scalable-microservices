9f7f5b320401990e856f2f4302e117837534e681
Sai Baba Nadh Konda <saibabanadh@gmail.com>
2019-12-20 00:46:09 +0530

    Deployment Conf updated.

--- install.sh
+++ install.sh
@@ -14,11 +14,22 @@ echo "----------------------------------------------------"
     cd -
 
     # Build Portal
+    mkdir -p frontend/certs
+    cd frontend/certs
+        openssl genrsa -out ss-key.pem 2048
+        openssl req -new -sha256 \
+            -subj "/C=IN/ST=Karnataka/L=Bangalore/O=Microservices/CN=scalableservices.com" \
+            -key ss-key.pem -out ss-csr.pem
+        openssl x509 -req -in ss-csr.pem -signkey ss-key.pem -out ss-cert.pem
+    cd -
+
     cd frontend
         bash ./buildPortal.sh
     cd -
     # Create & Start Containers using docker-compose
-    docker-compose up -d
+    docker network create scalable_network
+    docker-compose -f docker-compose-backend.yaml up -d
+    docker-compose -f docker-compose-frontend.yaml up -d
 
 echo "----------------------------------------------------"
 echo "*** Setup Completed ***"
