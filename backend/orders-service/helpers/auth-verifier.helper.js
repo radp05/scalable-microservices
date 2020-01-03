c1d8f2ab9321437eb8a1279e4b8328130c6bde1d
Pamujula Bala Subramanyam <pamujula.bs@incedoinc.com>
2020-01-03 14:32:39 +0530

    fixed statuscodes

--- backend/orders-service/helpers/auth-verifier.helper.js
+++ backend/orders-service/helpers/auth-verifier.helper.js
@@ -10,12 +10,18 @@ module.exports = (req, res, next) => {
         let bearerToken = req.headers.authorization.split(' ');
         jwt.verify(bearerToken[1], process.env.JWT_SECRET, function (err, decoded) {
             if (err) { 
-                res.send("You are not authorized");
+                res.status(401);
+                res.send({
+                    "msg":"You are not authorized"
+                })
             } else {
                 if ( decoded.resourceIds.includes(process.env.RESOURCE_ID) ) {
                     next()
                 } else {
-                    res.send("You are not authorized to access this API");
+                    res.status(401);
+                    res.send({
+                        "msg":"You are not authorized"
+                    })
                 }
             }
         });
